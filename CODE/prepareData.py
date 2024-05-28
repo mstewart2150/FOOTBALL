669da10a2f9fc1a70d37323d733251e111ee9337
mstewart2150 <33644856+mstewart2150@users.noreply.github.com>
2024-05-27 21:02:37 -0400

    Update

--- CODE/prepareData.py
+++ CODE/prepareData.py
@@ -1,5 +1,10 @@
 import nfl_data_py as nfl
 import numpy as np
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+#pd.reset_option('display.max_columns')
+
 
 year = [2023]
 df_depth = nfl.import_depth_charts(year)
@@ -7,3 +12,4 @@ df_pbp = nfl.import_pbp_data(year, downcast=True, cache=False, alt_path=None)
 
 
 df_qb_throw = df_pbp[df_pbp['time_to_throw'] != np.nan]
+df_qb_throw_summary = df_qb_throw.groupby(['season_type'])
