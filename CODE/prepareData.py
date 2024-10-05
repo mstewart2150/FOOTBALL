78991a719a28e1b45d40e0895cf91513b59f0428
mstewart2150 <33644856+mstewart2150@users.noreply.github.com>
2024-10-04 20:08:02 -0400

    Cleanup

--- CODE/prepareData.py
+++ CODE/prepareData.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 #pd.reset_option('display.max_columns')
 
 
@@ -12,4 +13,4 @@ df_pbp = nfl.import_pbp_data(year, downcast=True, cache=False, alt_path=None)
 
 
 df_qb_throw = df_pbp[df_pbp['time_to_throw'] != np.nan]
-df_qb_throw_summary = df_qb_throw.groupby(['season_type'])
+df_qb_throw_summary = df_qb_throw.groupby(['season_type', 'pass_length', 'passer_player_name'])['time_to_throw'].min().reset_index().sort_values(['time_to_throw', 'passer_player_name'])
